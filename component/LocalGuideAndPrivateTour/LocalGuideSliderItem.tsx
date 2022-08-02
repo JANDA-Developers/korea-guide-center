b0e8b9c0ec176e41d090cb30e3e883cd02c9e668
Jong jae Park <pjj186@naver.com>
2022-08-03 00:11:50 +0900

    LocalGuideSlider Zoom

--- component/LocalGuideAndPrivateTour/LocalGuideSliderItem.tsx
+++ component/LocalGuideAndPrivateTour/LocalGuideSliderItem.tsx
@@ -19,35 +19,35 @@ const LocalGuideSliderItem = ({
         <>
             {item.slice(offset * index, offset * index + offset).map((i) => {
                 return (
-                    <div
-                        className="slider__ShortSliderItems"
-                        style={{
-                            backgroundImage: `url(${i.imageUrl})`,
-                            backgroundSize: "cover",
-                        }}
-                    >
-                        <h5 className="slider__RegionSliderItemTitle">
-                            {i.title}
-                        </h5>
-                        <div
-                            style={{
-                                display: "flex",
-                                flexDirection: "column",
-                            }}
-                        >
-                            <span
+                    <div className="slider__ShortSliderItems">
+                        <img
+                            src={`${i.imageUrl}`}
+                            className="region__bgImage"
+                        />
+                        <div className="region__contents">
+                            <h5 className="region__RegionSliderItemTitle">
+                                {i.title}
+                            </h5>
+                            <div
                                 style={{
-                                    color: "white",
-                                    fontWeight: "bold",
-                                    marginBottom: "5px",
-                                    textShadow: "2px 2px 2px gray",
+                                    display: "flex",
+                                    flexDirection: "column",
                                 }}
                             >
-                                {i.desc}
-                            </span>
-                            <button className="region__RegionDetailButton">
-                                둘러보기
-                            </button>
+                                <span
+                                    style={{
+                                        color: "white",
+                                        fontWeight: "bold",
+                                        marginBottom: "5px",
+                                        textShadow: "2px 2px 2px gray",
+                                    }}
+                                >
+                                    {i.desc}
+                                </span>
+                                <button className="region__RegionDetailButton">
+                                    둘러보기
+                                </button>
+                            </div>
                         </div>
                     </div>
                 );
