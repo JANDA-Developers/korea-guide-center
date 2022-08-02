1c85ea00721b081fc1400289599d6171fd044115
Jong jae Park <pjj186@naver.com>
2022-08-02 23:54:06 +0900

    지역 Hover 시 Zoom 효과

--- component/TourAndActivities/RegionSliderItem.tsx
+++ component/TourAndActivities/RegionSliderItem.tsx
@@ -17,19 +17,19 @@ const TourSliderItem = ({ item, offset, index }: IRegionSliderItemProps) => {
         <>
             {item.slice(offset * index, offset * index + offset).map((i) => {
                 return (
-                    <div
-                        className="slider__ShortSliderItems"
-                        style={{
-                            backgroundImage: `url(${i.imageUrl})`,
-                            backgroundSize: "cover",
-                        }}
-                    >
-                        <h5 className="slider__RegionSliderItemTitle">
-                            {i.title}
-                        </h5>
-                        <button className="region__RegionDetailButton">
-                            둘러보기
-                        </button>
+                    <div className="slider__ShortSliderItems">
+                        <img
+                            src={`${i.imageUrl}`}
+                            className="region__bgImage"
+                        />
+                        <div className="region__contents">
+                            <h5 className="region__RegionSliderItemTitle">
+                                {i.title}
+                            </h5>
+                            <button className="region__RegionDetailButton">
+                                둘러보기
+                            </button>
+                        </div>
                     </div>
                 );
             })}
