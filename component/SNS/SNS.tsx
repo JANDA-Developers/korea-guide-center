ee8e4d978901e9b2631c1f52f2118f06c5bf0df7
Jong jae Park <pjj186@naver.com>
2022-08-03 17:15:58 +0900

    SNS

--- component/SNS/SNS.tsx
+++ component/SNS/SNS.tsx
@@ -1,101 +1,51 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-    width: 100%;
-    height: 35.25rem;
-    margin-top: 9.375rem;
-    display: flex;
-    justify-content: space-evenly;
-    align-items: center;
-
-    margin-bottom: 9.375rem;
-`;
-
-const Youtube = styled.div``;
-
-const Title = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    margin-bottom: 10px;
-`;
-
-const LogoAndText = styled.div`
-    display: flex;
-    align-items: center;
-`;
-
-const TitleText = styled.span`
-    margin-left: 10px;
-    font-weight: 600;
-`;
-
-const Instagram = styled.div``;
-
-const Image = styled.div`
-    width: 500px;
-    height: 500px;
-    background-color: red;
-`;
-
-const SubButton = styled.a`
-    background-color: red;
-    color: white;
-    font-weight: 600;
-    padding: 5px 20px;
-    border-radius: 10px;
-`;
-
-const FollowButton = styled.a`
-    background-color: #1877f2;
-    color: white;
-    font-weight: 600;
-    padding: 5px 20px;
-    border-radius: 10px;
-`;
-
 const SNS = () => {
     return (
-        <Container>
-            <Youtube>
-                <Title>
-                    <LogoAndText>
+        <div className="sns__Container">
+            <div>
+                <div className="sns__Title">
+                    <div className="sns__LogoAndTextBox">
                         <img
                             src="img/sns/youtubeIcon.png"
                             width="45px"
                             height="35px"
                         />
-                        <TitleText>코리아 가이드 유튜브</TitleText>
-                    </LogoAndText>
-                    <SubButton
+                        <span className="sns__TitleText">
+                            코리아 가이드 유튜브
+                        </span>
+                    </div>
+                    <a
+                        className="sns__subButton"
                         href="https://www.youtube.com/channel/UCaqfo9iu08Nz53fri_oHmCQ/videos"
                         target="_blank"
                     >
                         구독하기
-                    </SubButton>
-                </Title>
-                <Image />
-            </Youtube>
-            <Instagram>
-                <Title>
-                    <LogoAndText>
+                    </a>
+                </div>
+                <img className="sns__image" src="img/sns/youtubeImage.jpeg" />
+            </div>
+            <div>
+                <div className="sns__Title">
+                    <div className="sns__LogoAndTextBox">
                         <img
                             src="img/sns/instagramIcon.png"
                             width="50px"
                             height="50px"
                         />
-                        <TitleText>코리아 가이드 인스타그램</TitleText>
-                    </LogoAndText>
-                    <FollowButton
+                        <span className="sns__TitleText">
+                            코리아 가이드 인스타그램
+                        </span>
+                    </div>
+                    <a
+                        className="sns__followButton"
                         href="https://www.instagram.com/korea_guide_/"
                         target="_blank"
                     >
                         팔로우
-                    </FollowButton>
-                </Title>
-                <Image />
-            </Instagram>
-        </Container>
+                    </a>
+                </div>
+                <img className="sns__image" src="img/sns/instagramImage.jpeg" />
+            </div>
+        </div>
     );
 };
 
