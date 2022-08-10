f35ee3d57e32e15eef5f5cc799694a007881aa54
Jong jae Park <pjj186@naver.com>
2022-08-10 16:30:55 +0900

    [locationalGuide] import 정리

--- pages/locationalGuide.tsx
+++ pages/locationalGuide.tsx
@@ -1,38 +1,9 @@
-import { Flex, JDcontainer, JDtypho, Mb, WindowSize } from "@janda-com/front";
-import Image from "next/image";
-import { useRouter } from "next/router";
-import React, { useEffect, useRef } from "react";
-import { useContext } from "react";
-import { Banner } from "../component/banner/Banner";
-import {
-    GuideMovieCardsWithApi,
-    HyperRegionByGuideViewCarsGroup,
-    HyperRegionByGuideViewCarsGroup2,
-} from "../component/guideMovieClicp/GuideMovieClipList";
-import { KoreaMap } from "../component/koreaMap/KoreaMap";
+import { WindowSize } from "@janda-com/front";
+import React from "react";
 import { BookLayout } from "../component/layout/BookLayout";
-import {
-    ProductViewCardsWithApi,
-    regionIn,
-} from "../component/productViewCard/ProductViewCards";
-import {
-    BestProductViewsLineHeader,
-    NewstProductViewsLineHeader,
-    ProductViewsLineHeader,
-    RegionProductViewsLineHeader,
-    RegionProductViewsLineHeader2,
-} from "../component/productViewCard/ProductViewsLineHeader";
-import {
-    RegionDescriptionSecotr,
-    RegionDescriptionSecotr2,
-    RegionDescriptionSecotr3,
-} from "../component/RegionDescriptionSector/RegionDescriptionSector";
 import { SelectRegionSector } from "../component/SelectRegionSector/SelectRegionSector";
-import { AppContext } from "../context/context";
 import { useGlobalKoreaMap, useKoreaMap } from "../hook/useKoreaMap";
-import { useSortBanner } from "../page/homepage/hook/useSortBanner";
 import { localGuideData } from "../component/LocalGuideAndPrivateTour/LocalGuideSlider";
-import { FileTagManager } from "../utils/tagManager";
 import RegionTopImage from "../component/RegionTopImage/RegionTopImage";
 import RegionGuides from "../component/RegionGuides/RegionGuides";
 
@@ -62,15 +33,13 @@ export const LocationalGuide: React.FC<IProp> = () => {
                         region={selectedGlobalRegion}
                     />
                     {selectedGlobalRegion && (
-                        <JDcontainer size={containerSize}>
-                            <div className="locationalGuide__regionSelectorSectorContainer">
-                                <SelectRegionSector
-                                    onSelectRegion={selectGlobalRegion}
-                                    region={selectedGlobalRegion}
-                                    items={localGuideData}
-                                />
-                            </div>
-                        </JDcontainer>
+                        <div className="locationalGuide__regionSelectorSectorContainer">
+                            <SelectRegionSector
+                                onSelectRegion={selectGlobalRegion}
+                                region={selectedGlobalRegion}
+                                items={localGuideData}
+                            />
+                        </div>
                     )}
                 </div>
                 <RegionGuides
