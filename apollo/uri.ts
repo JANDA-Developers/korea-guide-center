21505427b92a70a42ab6574892bff8343454582f
JongJae Park <pjj186@naver.com>
2022-09-29 11:05:12 +0900

    ay

--- apollo/uri.ts
+++ apollo/uri.ts
@@ -1,4 +1,10 @@
-export const SERVER_URI = `${process.env.NEXT_PUBLIC_SERVER_URI}/graphql`;
+const IP = "http://10.159.1.17";
+const NAME = "http://localhost";
+export const LOCAL_IP = NAME;
+const LOCAL = `${LOCAL_IP}:4000/graphql`;
+const PROD = "https://api.koreaguide.center/graphql";
+
+export const SERVER_URI = process.env.NODE_ENV === "development" ? LOCAL : PROD;
 
 export default (() => {
     console.log("Listening: " + SERVER_URI);
