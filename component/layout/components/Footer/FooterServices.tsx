1062d56e279d1f6a46ad70fe6625aca4d2f5bb30
JongJae Park <pjj186@naver.com>
2023-01-11 14:40:36 +0900

    FooterService

--- component/layout/components/Footer/FooterServices.tsx
+++ component/layout/components/Footer/FooterServices.tsx
@@ -1,63 +1,129 @@
-import React from "react";
-import Footer from "./Footer";
+import React, { useContext } from "react";
+import { AppContext } from "../../../../context/context";
 
 const FooterServices = () => {
+    const { s } = useContext(AppContext);
     return (
         <div className="col-25">
-            <span className="footer-title">Services</span>
+            <span className="footer-title"> {s("footerServices")}</span>
             <ul>
                 <li>
-                    <a href="https://www.neweuropetours.eu/coronavirus-update/">
-                        COVID-19 Update
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=DRIVING`;
+                        }}
+                    >
+                        {s("ServicesDriving")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/guide-with-us/">
-                        Guide with us
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=MICE`;
+                        }}
+                    >
+                        {s("ServicesMICE")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/terms-and-conditions/">
-                        Terms and Conditions
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=통역`;
+                        }}
+                    >
+                        {s("ServicesTranlate")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/blog/">
-                        SANDEMANs Blog
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=Barrier-Free`;
+                        }}
+                    >
+                        {s("ServicesBarrierFree")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/faqs/">FAQs</a>
-                </li>
-                <li>
-                    <a href="https://www.neweuropetours.eu/loyalty-program/">
-                        Loyalty Program
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=VIP의전`;
+                        }}
+                    >
+                        {s("ServicesVIP")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/vip-program/">
-                        VIP Program
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=의료관광`;
+                        }}
+                    >
+                        {s("ServicesWellness")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/gender-equality-at-sandemans/">
-                        Gender Equality
+                    <a
+                        onClick={() => {
+                            location.href = `/offer`;
+                        }}
+                    >
+                        {s("ServicesPrivate")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/press/">Press</a>
-                </li>
-                <li>
-                    <a href="https://www.neweuropetours.eu/about-us/">
-                        About Us
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=부동산`;
+                        }}
+                    >
+                        {s("ServicesRealEstate")}
                     </a>
                 </li>
                 <li>
-                    <a href="https://sandemansneweurope.recruitee.com/">Jobs</a>
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=유학`;
+                        }}
+                    >
+                        {s("ServicesStudyingAbroad")}
+                    </a>
                 </li>
                 <li>
-                    <a href="https://www.neweuropetours.eu/umbrella/">
-                        Umbrella
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=장기체류`;
+                        }}
+                    >
+                        {s("ServicesLongStay")}
+                    </a>
+                </li>
+                <li>
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=요리`;
+                        }}
+                    >
+                        {s("ServicesCook")}
+                    </a>
+                </li>
+                <li>
+                    <a
+                        onClick={() => {
+                            location.href = `cities/search?title=${s(
+                                "ServicesCustom"
+                            )}`;
+                        }}
+                    >
+                        {s("ServicesCustom")}
+                    </a>
+                </li>
+                <li>
+                    <a
+                        onClick={() => {
+                            location.href = `/cities/search?title=축제`;
+                        }}
+                    >
+                        {s("ServicesLocalFestival")}
                     </a>
                 </li>
             </ul>
