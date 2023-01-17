9c851c1f8e7a97e1c617930ba04fa9202338b93f
JongJae Park <pjj186@naver.com>
2023-01-17 15:06:47 +0900

    태블릿 환경에서 검색 아이콘 클릭 시 검색페이지에서 검색창이 안뜨는 현상 ✅

--- utils/isMobile.ts
+++ utils/isMobile.ts
@@ -1,7 +1,7 @@
 export const checkMobile = () => {
     let isMobile = false;
     if (typeof window !== "undefined") {
-        isMobile = window.outerWidth < 500;
+        isMobile = window.outerWidth < 1024;
     }
     return isMobile;
 };
