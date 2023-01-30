592430244621c65987d7aad02341eb23a25e1456
sunokryu <sunokryu319@gmail.com>
2023-01-30 19:46:59 +0900

    korea guide title center

--- component/layout/components/RegionTourSlider/RegionTourSliderTextIntro.tsx
+++ component/layout/components/RegionTourSlider/RegionTourSliderTextIntro.tsx
@@ -2,8 +2,16 @@ import React from "react";
 
 const RegionTourSliderTextIntro = () => {
     return (
-        <div className="txt-intro">
-            <h2>
+        <div className="txt-intro" style={{
+            margin: 0,
+            marginBottom: "20px",
+            width: "100%",
+            display: "flex",
+            justifyContent: "center",
+        }}>
+            <h2 style={{
+                width: "fit-content"
+            }}>
                 <strong>KOREA GUIDE</strong>
             </h2>
         </div>
