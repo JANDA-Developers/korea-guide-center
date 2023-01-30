d1ce9e0962395a57e6d3e4b2bab43e616182e62e
sunokryu <sunokryu319@gmail.com>
2023-01-30 19:50:33 +0900

    by region title 가운데로

--- component/layout/components/RegionalGuide/RegionalGuideTextIntro.tsx
+++ component/layout/components/RegionalGuide/RegionalGuideTextIntro.tsx
@@ -5,10 +5,15 @@ const RegionalGuideTextIntro = () => {
         <div
             className="txt-intro"
             style={{
-                textAlign: "left",
-            }}
-        >
-            <h2>
+                margin: 0,
+                marginBottom: "20px",
+                width: "100%",
+                display: "flex",
+                justifyContent: "center",
+            }}>
+            <h2 style={{
+                width: "fit-content"
+            }}>
                 <strong>GUIDES</strong> BY REGION
             </h2>
         </div>
