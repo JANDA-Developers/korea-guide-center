05e1cdfcad4d9b25c7716de5b830840698e99b6e
AbletonPilot <sonjm0428@gmail.com>
2023-02-06 15:42:12 +0900

    메인페이지 유튜브 레이아웃 완료(반응형 ok)

--- component/layout/components/Social/SocialYoutube.tsx
+++ component/layout/components/Social/SocialYoutube.tsx
@@ -4,29 +4,93 @@ const SocialYouTube = () => {
     return (
         <div className="col-66 facebook">
             <span className="title">
-                <img src="img/sns/youtubeIcon.png" width="35px" height="25px" />
-                YOUTUBE
-                <a
+                <div className="youtubeChannel">
+                    <img
+                        src="img/sns/youtubeIcon.png"
+                        width="65px"
+                        height="60px"
+                    />
+                    <div>
+                        <div className="youtubeChannelName">YOUTUBE</div>
+                        {/* <a
                     href="https://www.youtube.com/channel/UCaqfo9iu08Nz53fri_oHmCQ/videos"
                     target="_blank"
                     rel="nofollow noreferrer"
                 >
                     Korea Guide
-                </a>
+                </a> */}
+                        <span className="youtubeSubscribe">구독자 25만명</span>
+                    </div>
+                </div>
             </span>
-            <a
-                target="_blank"
-                href="https://www.youtube.com/channel/UCaqfo9iu08Nz53fri_oHmCQ/videos"
-                rel="noopener noreferrer"
-            >
-                <figure className="fit-cover">
-                    <img
-                        src="/img/sns/youtubeImage.jpeg"
-                        alt=""
-                        className="lazy"
-                    />
-                </figure>
-            </a>
+            <div className="youtubeContents">
+                <div className="youtubeContentsCard">
+                    {/* <a
+                        target="_blank"
+                        href="https://www.instagram.com/p/CaYXZA-v4pQ/"
+                        rel="noopener noreferrer"
+                    > */}
+                    <figure className="mainFitCover">
+                        <img
+                            src="/img/sns/maxresdefault.jpg"
+                            alt=""
+                            className="mainFitCoverYoutube"
+                        />
+                    </figure>
+                    {/* </a> */}
+                    <div className="youtubeTitle">
+                        <span>제목 타이틀</span>
+                        <div className="youtubeStatistics">
+                            <span>조회수 25만회</span>
+                            <span> ∙ 1달전</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="youtubeContentsCard">
+                    {/* <a
+                        target="_blank"
+                        href="https://www.instagram.com/p/CgMgsxxP-M3/"
+                        rel="noopener noreferrer"
+                    > */}
+                    <figure className="mainFitCover">
+                        <img
+                            src="/img/sns/maxresdefault.jpg"
+                            alt=""
+                            className="mainFitCoverYoutube"
+                        />
+                    </figure>
+                    {/* </a> */}
+                    <div className="youtubeTitle">
+                        <span>제목 타이틀</span>
+                        <div className="youtubeStatistics">
+                            <span>조회수 25만회</span>
+                            <span> ∙ 1달전</span>
+                        </div>
+                    </div>
+                </div>
+                <div className="youtubeContentsCard">
+                    {/* <a
+                        target="_blank"
+                        href="https://www.instagram.com/p/CagutJWlkOO/"
+                        rel="noopener noreferrer"
+                    > */}
+                    <figure className="mainFitCover">
+                        <img
+                            src="/img/sns/maxresdefault.jpg"
+                            alt=""
+                            className="mainFitCoverYoutube"
+                        />
+                    </figure>
+                    {/* </a> */}
+                    <div className="youtubeTitle">
+                        <span>제목 타이틀</span>
+                        <div className="youtubeStatistics">
+                            <span>조회수 25만회</span>
+                            <span> ∙ 1달전</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
